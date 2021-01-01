be5543d5f6bd3b722b2ffad5a316ef6ac482a7b6
FIGBERT <figbert@figbert.com>
2020-12-31 17:00:00 -0800

    Add spin script to sevivon node
    The spin script monitors the accelerometer to
    detect a shake. When the user shakes the device,
    the sevivon/dreidel tilts slightly on the X and Z
    axes and significant torque is applied to the Y
    axis.

---
+++ scripts/sevivon.gd
@@ -0,0 +1,16 @@
+extends RigidBody
+
+const ACCEL_THRESHOLD = 3
+const MIN_ANGLE = 0.0
+const MAX_ANGLE = 0.1
+const MIN_FORCE = 5
+const MAX_FORCE = 12
+var has_spun = false
+
+func _physics_process(delta):
+	var accel = Input.get_accelerometer()
+	if accel.length() > ACCEL_THRESHOLD:
+		randomize()
+		angular_velocity = Vector3(rand_range(MIN_ANGLE, MAX_ANGLE), 0, rand_range(MIN_ANGLE, MAX_ANGLE))
+		apply_torque_impulse(Vector3.UP * accel.length() * rand_range(MIN_FORCE, MAX_FORCE))
+		has_spun = true
