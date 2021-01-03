ed99dfe498b8a569c7e5ffd4717d08f8a10535e2
FIGBERT <figbert@figbert.com>
2021-01-03 15:52:01 -0800

    Separate sevivon spin code to a spin function

--- godot/scripts/sevivon.gd
+++ godot/scripts/sevivon.gd
@@ -10,7 +10,10 @@ var has_spun = false
 func _physics_process(_delta):
 	var accel = Input.get_accelerometer()
 	if accel.length() > ACCEL_THRESHOLD:
-		randomize()
-		angular_velocity = Vector3(rand_range(MIN_ANGLE, MAX_ANGLE), 0, rand_range(MIN_ANGLE, MAX_ANGLE))
-		apply_torque_impulse(Vector3.UP * accel.length() * rand_range(MIN_FORCE, MAX_FORCE))
-		has_spun = true
+		spin(accel)
+
+func spin(accel: Vector3):
+	randomize()
+	angular_velocity = Vector3(rand_range(MIN_ANGLE, MAX_ANGLE), 0, rand_range(MIN_ANGLE, MAX_ANGLE))
+	apply_torque_impulse(Vector3.UP * accel.length() * rand_range(MIN_FORCE, MAX_FORCE))
+	has_spun = true
