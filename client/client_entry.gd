7d4e161cb0aa972f06061f578bdc6af01a08aa6a
FIGBERT <figbert@figbert.com>
2021-04-06 20:57:15 -0700

    Merge loading scene into client_entry scene

--- client/client_entry.gd
+++ client/client_entry.gd
@@ -1,13 +1,31 @@
-extends Node
+extends Control
+
+
+const LOADING_MESSAGE := "Connecting to server"
+const FAILURE_MESSAGE := "Connection to server failed."
+var safe_area := OS.get_window_safe_area()
+var connection_failed := false
+var dots := 0
 
 
 func _ready() -> void:
-	var loading_scene: Node = load("res://client/loading/loading.tscn").instance()
-	add_child(loading_scene)
+	self.set_margin(MARGIN_TOP, safe_area.position.y)
 	Network.initialize_network()
-	get_tree().connect("connected_to_server", self, "_client_connected_successfully")
-	get_tree().connect("connection_failed", $LoadingScreen, "failure")
+	get_tree().connect("connected_to_server", self, "_connection_successful")
+	get_tree().connect("connection_failed", self, "_connection_failed")
+	while not connection_failed:
+		$Message.text = LOADING_MESSAGE + ".".repeat(dots)
+		yield(get_tree().create_timer(0.5), "timeout")
+		if dots < 3:
+			dots += 1
+		else:
+			dots = 0
 
 
-func _client_connected_successfully() -> void:
+func _connection_successful() -> void:
 	get_tree().change_scene("res://client/gameplay/gameplay.tscn")
+
+
+func _connection_failed() -> void:
+	$Message.text = FAILURE_MESSAGE
+	connection_failed = true
