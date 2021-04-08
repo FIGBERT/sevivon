62ec60996be73729f7958163f261288c07abb818
FIGBERT <figbert@figbert.com>
2021-04-08 13:47:54 -0700

    Create Player class to replace dictionary
    Previously, code just assumed that the dictionary
    conformed to the structure outlined in state.gd.
    With the addition of the Player class, properties
    can be typed and well defined.
    
    Now, if only Godot had types for array members...
    https://github.com/godotengine/godot-proposals/issues/192

--- common/state.gd
+++ common/state.gd
@@ -1,22 +1,8 @@
 extends Node
 
+
 const USERNAMES := ["Judah", "Yochanan", "Shimon", "Elazar", "Yonatan"]
 const PLAYER_STARTING_GELT := 10
-
-# A dictionary containing all the current players.
-# Each player is assigned an integer key, equal to
-# their network unique id. The value of each key
-# is another dictionary, containing a number of
-# properties:
-#
-# name: String = player's username.
-# gelt: int = quantity of player's gelt.
-# ready: bool = whether or not the player is ready
-#               to begin the game. `true` after the
-#               game has begun.
-# in: bool = whether or not the player is in or out.
-# paid_ante: bool = whether or not the player has
-#                   paid all required antes.
 remotesync var players := {}
 
 
@@ -25,24 +11,31 @@ func reset_state() -> void:
 
 
 func add_player(id: int) -> void:
-	var _players := players.duplicate(true)
-	_players[id] = {
-		"name": USERNAMES[players.size()],
-		"gelt": PLAYER_STARTING_GELT,
-		"ready": false,
-		"in": true,
-		"paid_ante": true,
-	}
+	var _players := players.duplicate()
+	_players[id] = Player.new(id, USERNAMES[players.size()])
 	rset("players", _players)
 
 
 func remove_player(id: int) -> void:
-	var _players := players.duplicate(true)
+	var _players := players.duplicate()
 	_players.erase(id)
 	rset("players", _players)
 
 
 func make_player_ready(id: int) -> void:
-	var _players := players.duplicate(true)
-	_players[id]["ready"] = true
+	var _players := players.duplicate()
+	_players[id].ready = true
 	rset("players", _players)
+
+
+class Player:
+	var id: int
+	var username: String
+	var gelt := PLAYER_STARTING_GELT
+	var ready := false
+	var in_game := true
+	var paid_ante := true
+	
+	func _init(_id: int, _username: String) -> void:
+		id = _id
+		username = _username
