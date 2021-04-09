476685a78199fd907c9e23f79b6cf70f2fcb4629
FIGBERT <figbert@figbert.com>
2021-04-08 17:36:07 -0700

    Don't name the sevivon/dreidel after the client id
    Players will take turns spinning the same sevivon/dreidel,
    and so naming it after the client doesn't make sense.

--- client/lobby/client_lobby.gd
+++ client/lobby/client_lobby.gd
@@ -3,7 +3,6 @@ extends Node
 
 func _ready() -> void:
 	var me: Node = load("res://client/components/sevivon/sevivon.tscn").instance()
-	me.set_name(str(get_tree().get_network_unique_id()))
 	me.translate(Vector3.BACK * 3)
 	get_tree().get_root().add_child(me)
 
