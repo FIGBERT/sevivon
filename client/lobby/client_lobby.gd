79920cb29e45c7960fc6e238c16398620d5e8a6c
FIGBERT <figbert@figbert.com>
2021-04-08 17:21:08 -0700

    Merge players.gd into client_lobby.gd

--- client/lobby/client_lobby.gd
+++ client/lobby/client_lobby.gd
@@ -1,12 +1,10 @@
 extends Node
 
 
-onready var players := preload("res://client/components/gameplay/players.gd").new(
-		get_tree().get_network_unique_id())
-
-
 func _ready() -> void:
-	var me: Node = players.create_self()
+	var me: Node = load("res://client/components/sevivon/sevivon.tscn").instance()
+	me.set_name(str(get_tree().get_network_unique_id()))
+	me.translate(Vector3.BACK * 3)
 	get_tree().get_root().add_child(me)
 
 
